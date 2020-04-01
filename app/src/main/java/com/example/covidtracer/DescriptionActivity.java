2d0d856c784b0301b8738f28c36b02895c54baf1
Alexandru Meterez <alexandrumeterez@gmail.com>
2020-04-01 23:55:45 +0300

    fixed layout on first activity

--- app/src/main/java/com/example/covidtracer/DescriptionActivity.java
+++ app/src/main/java/com/example/covidtracer/DescriptionActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class DescriptionActivity extends AppCompatActivity {
@@ -14,7 +15,6 @@ public class DescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnRegister = findViewById(R.id.btn_register);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
