6499a3757ee040e858bdd3a46d4a1346c6e2e777
Alexandru Meterez <alexandrumeterez@gmail.com>
2020-04-02 01:20:25 +0300

    made the logged in activity look much better

--- app/src/main/java/com/example/covidtracer/LoggedInActivity.java
+++ app/src/main/java/com/example/covidtracer/LoggedInActivity.java
@@ -1,37 +1,45 @@
 package com.example.covidtracer;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.covidtracer.dbhelpers.FirebaseDatabaseHelper;
 
 public class LoggedInActivity extends AppCompatActivity {
-    private TextView currentStatus;
     private String TAG = "LoggedInActivity";
     private final String[] statuses = {"Sanatos", "Autoizolat", "Diagnosticat"};
+    private NumberPicker picker;
+    private Chronometer chronometer;
+    private Button button;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
 
-        currentStatus = findViewById(R.id.textCurrentStatus);
+        picker = findViewById(R.id.pickerHealthStatus);
+        chronometer = findViewById(R.id.chronometer);
+        button = findViewById(R.id.btnUpdateHealthStatus);
+        progressBar = findViewById(R.id.progressBar);
+        picker.setMinValue(0);
+        picker.setMaxValue(statuses.length - 1);
+        picker.setDisplayedValues(statuses);
 
         final SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         final String strUserUID = sharedPreferences.getString(getString(R.string.UID), "None");
         String token = sharedPreferences.getString(getString(R.string.token), "None");
-        currentStatus.setText(sharedPreferences.getString(getString(R.string.status), "None"));
-        Log.d(TAG, currentStatus.getText().toString());
 
         FirebaseDatabaseHelper.getInstance().updateDeviceToken(strUserUID, token, new FirebaseDatabaseHelper.DataStatus() {
             @Override
@@ -44,39 +52,48 @@ public class LoggedInActivity extends AppCompatActivity {
                 Log.d(TAG, "Failed to save token");
             }
         });
-        currentStatus.setOnClickListener(new View.OnClickListener() {
+
+        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                String oldStatus = statuses[oldVal];
+                String newStatus = statuses[newVal];
+                String currentStatus = sharedPreferences.getString(getString(R.string.status), "None");
+                if (currentStatus.equals(newStatus)) {
+                    button.setEnabled(false);
+                } else {
+                    button.setEnabled(true);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoggedInActivity.this);
-                builder.setTitle("Ce status aveti?");
-                builder.setItems(statuses, new DialogInterface.OnClickListener() {
+                progressBar.setVisibility(View.VISIBLE);
+                FirebaseDatabaseHelper.getInstance().updateUserStatus(strUserUID, statuses[picker.getValue()], new FirebaseDatabaseHelper.DataStatus() {
                     @Override
-                    public void onClick(DialogInterface dialog, final int which) {
-                        if (!currentStatus.getText().equals(statuses[which])) {
-                            FirebaseDatabaseHelper.getInstance().updateUserStatus(strUserUID, statuses[which], new FirebaseDatabaseHelper.DataStatus() {
-                                @Override
-                                public void Success() {
-                                    Toast.makeText(LoggedInActivity.this, "Updated status", Toast.LENGTH_LONG).show();
-                                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                                    editor.putString(getString(R.string.status), statuses[which]);
-                                    editor.commit();
-                                    currentStatus.setText(statuses[which]);
-                                }
+                    public void Success() {
+                        Toast.makeText(LoggedInActivity.this, "Updated status", Toast.LENGTH_LONG).show();
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(getString(R.string.status), statuses[picker.getValue()]);
+                        editor.commit();
+                        button.setEnabled(false);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
 
-                                @Override
-                                public void Fail() {
-                                    Toast.makeText(LoggedInActivity.this, "Failed to update status", Toast.LENGTH_LONG).show();
-                                }
-                            });
-                        }
+                    @Override
+                    public void Fail() {
+                        Toast.makeText(LoggedInActivity.this, "Failed to update status", Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.INVISIBLE);
                     }
                 });
-                builder.show();
             }
         });
 
         startService(new Intent(this, NearbyTrackingService.class));
         startService(new Intent(this, CustomFirebaseMessagingService.class));
+        chronometer.start();
     }
 
     @Override
@@ -84,5 +101,6 @@ public class LoggedInActivity extends AppCompatActivity {
         super.onDestroy();
         stopService(new Intent(this, NearbyTrackingService.class));
         stopService(new Intent(this, CustomFirebaseMessagingService.class));
+        chronometer.stop();
     }
 }
