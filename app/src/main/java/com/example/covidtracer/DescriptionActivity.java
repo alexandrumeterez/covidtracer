45a988f20c50c2c49a5b8525bc9234e64c88b9e4
Alexandru Meterez <alexandrumeterez@gmail.com>
2020-04-02 15:40:27 +0300

    added permissions on startup

--- app/src/main/java/com/example/covidtracer/DescriptionActivity.java
+++ app/src/main/java/com/example/covidtracer/DescriptionActivity.java
@@ -1,8 +1,14 @@
 package com.example.covidtracer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -10,18 +16,79 @@ import android.widget.Button;
 
 public class DescriptionActivity extends AppCompatActivity {
     private Button btnRegister;
+    private Context context;
+    public static final int MULTIPLE_PERMISSIONS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         btnRegister = findViewById(R.id.btn_register);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DescriptionActivity.this, PhoneRegisterActivity.class);
-                startActivity(intent);
+                checkPermission();
             }
         });
+    }
+
+
+    // function to check permissions
+    private void checkPermission() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED)) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (this, Manifest.permission.INTERNET) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (this, Manifest.permission.FOREGROUND_SERVICE)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(
+                            new String[]{Manifest.permission
+                                    .INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.FOREGROUND_SERVICE},
+                            MULTIPLE_PERMISSIONS);
+                }
+
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(
+                            new String[]{Manifest.permission
+                                    .INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.FOREGROUND_SERVICE},
+                            MULTIPLE_PERMISSIONS);
+                }
+            }
+        }
+    }
+
+
+    // Function to initiate after permissions are given by user
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS:
+                if (grantResults.length > 0) {
+                    boolean internetPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean accesFineLocationPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean foregroundServicePermission = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                    if (internetPermission && accesFineLocationPermission && foregroundServicePermission) {
+                        Intent intent = new Intent(DescriptionActivity.this, PhoneRegisterActivity.class);
+                        startActivity(intent);
+                    }
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(
+                                new String[]{Manifest.permission
+                                        .INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.FOREGROUND_SERVICE},
+                                MULTIPLE_PERMISSIONS);
+                    }
+                }
+
+        }
     }
 }
