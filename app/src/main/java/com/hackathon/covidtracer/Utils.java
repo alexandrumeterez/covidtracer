fe058b9f30ddf8dce20cd6106783dcb662061dae
NiekBijman <niekbijman@gmail.com>
2020-04-04 21:54:40 +0200

    Add store / retrieve arraylist from sharedpreferences

---
+++ app/src/main/java/com/hackathon/covidtracer/Utils.java
@@ -0,0 +1,30 @@
+package com.hackathon.covidtracer;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class Utils {
+    public static void saveArrayList(Context context, String key, ArrayList<String> list){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString(key, json);
+        editor.apply();     // This line is IMPORTANT !!!
+    }
+
+    public static ArrayList<String> getArrayList(String key, Context context){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Gson gson = new Gson();
+        String json = prefs.getString(key, null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+}
